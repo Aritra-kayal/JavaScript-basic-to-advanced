0c0fc0b886433010b123eb617860625fb4660135
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-12 23:41:47 +0530

    operater in js

---
+++ part-2/operators.js
@@ -0,0 +1,21 @@
+//Arithmatic operator
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+//Unary operator
+console.log(a++);
+console.log(++a);
+console.log(b--);
+console.log(--b);
+//Assignment operator
+let c = 10;
+console.log((c += 1));
+console.log((c -= 1));
+console.log((c *= 1));
+console.log((c /= 1));
+console.log((c %= 1));
