98d5c0348fc07129e191c751b366f3b53aad81e8
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-12 23:56:43 +0530

    comparison operators in JS

---
+++ part-2/comparisonOperators.js
@@ -0,0 +1,8 @@
+let age = 23;
+console.log(age > 18);
+console.log(age < 18);
+let age1 = 18;
+console.log(age1 <= 18);
+console.log(age1 >= 18);
+console.log(age == age1);
+console.log(age != age1);
