02985855a73193995fdecaef099c8091ebc9b8bd
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 23:47:07 +0530

    prompt in JS

---
+++ part-2/prompt.js
@@ -0,0 +1,7 @@
+// let firstName = prompt("Enter your first name: ");
+// console.log(firstName);
+
+let firstName = prompt("Enter your first name: ");
+let lastName = prompt("Enter your last name: ");
+console.log(`My name is ${firstName + " " + lastName}`);
+// console.log("My name is", firstName, lastName);
