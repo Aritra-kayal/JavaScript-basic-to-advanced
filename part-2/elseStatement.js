8bb5a9307149d1f4343fd3d6e55d6988a568911e
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 16:49:58 +0530

    else statement in JS

---
+++ part-2/elseStatement.js
@@ -0,0 +1,19 @@
+let age = 14;
+if (age >= 18) {
+  console.log("You can vote");
+} else {
+  console.log("You cannot vote");
+}
+
+/*
+let color = "blue";
+if (color === "red") {
+  console.log("Stop");
+} else if (color === "yellow") {
+  console.log("Slow down");
+} else if (color === "green") {
+  console.log("Go");
+} else {
+  console.log("traffic light is broken");
+}
+*/
