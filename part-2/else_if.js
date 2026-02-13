eecc2c1102263262710eadceb94674dbf293fc9a
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 16:22:31 +0530

    else if in JS

---
+++ part-2/else_if.js
@@ -0,0 +1,50 @@
+let age = 14;
+if (age >= 18) {
+  console.log("You can vote");
+} else if (age < 18) {
+  console.log("You cannot vote");
+}
+
+/*
+let age = 14;
+if (age > 18) {
+  console.log("You can vote");
+} else if (age == 18) {
+  console.log("Not adult");
+} else if (age < 18) {
+  console.log("You cannot vote");
+}
+*/
+
+/*
+let marks = 75;
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 60) {
+  console.log("A");
+} else if (marks >= 33) {
+  console.log("B");
+} else if (marks < 33) {
+  console.log("F");
+}
+*/
+
+/*
+let month = "january";
+if (month === "january") {
+  console.log("Winter is here");
+} else if (month === "april") {
+  console.log("Sumer is here");
+}
+*/
+
+/*
+let color = "green";
+if (color === "red") {
+  console.log("Stop");
+} else if (color === "yellow") {
+  console.log("Slow down");
+} else if (color === "green") {
+  console.log("Go");
+}
+*/
