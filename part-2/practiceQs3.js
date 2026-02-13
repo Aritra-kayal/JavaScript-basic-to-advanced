7b0adc50d279c6c9d65c25e4028107c27b6037cd
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 19:11:21 +0530

    practice Qs3

---
+++ part-2/practiceQs3.js
@@ -0,0 +1,8 @@
+let str = "apple";
+// let chr = str[0];
+// let len = str.length;
+if (str[0] === "a" && str.length > 3) {
+  console.log("good string");
+} else {
+  console.log("Not good string");
+}
