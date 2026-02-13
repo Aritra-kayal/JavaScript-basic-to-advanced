318ba3e7ec663687a4b5b3abadde53192d841bae
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 21:09:46 +0530

    truthy and falsy in JS

---
+++ part-2/truthyAndFalsy.js
@@ -0,0 +1,100 @@
+if (true) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (false) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (1) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (0) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (-0) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if ("Apna College") {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (null) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (NaN) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (undefined) {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if ("") {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+if (" ") {
+  console.log("it has true value");
+} else {
+  console.log("it has false value");
+}
+
+let string = "";
+if (string) {
+  console.log("String is not empty");
+} else {
+  console.log("String is empty");
+}
+
+let string1 = " ";
+if (string1) {
+  console.log("String is not empty");
+} else {
+  console.log("String is empty");
+}
+
+let string2 = "a";
+if (string2) {
+  console.log("String is not empty");
+} else {
+  console.log("String is empty");
+}
+
+let num = 0;
+if (num) {
+  console.log("num is not eqal to 0");
+} else {
+  console.log("num is eqal to 0");
+}
+
+let num1 = -10;
+if (num1) {
+  console.log("num is not eqal to 0");
+} else {
+  console.log("num is eqal to 0");
+}
