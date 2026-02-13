fdefb6bd4a673f632703b4a5fdcc9849b06626a3
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-13 14:40:39 +0530

    if statement in JS

---
+++ part-2/ifStatement.js
@@ -0,0 +1,68 @@
+console.log("before my if statement");
+let age = 23;
+if (age >= 18) {
+  console.log("You can vote");
+}
+console.log("after my if statement");
+
+/*
+console.log("before my if statement");
+let age = 23;
+if (age >= 18) {
+  console.log("You can vote");
+  console.log("You can drive");
+  let a = 5;
+  console.log(a * 5);
+}
+console.log("after my if statement");
+*/
+
+/*
+console.log("before my if statement");
+let age = 14;
+if (age >= 18) {
+  console.log("You can vote");
+  console.log("You can drive");
+  let a = 5;
+  console.log(a * 5);
+}
+console.log("after my if statement");
+*/
+
+/*
+console.log("before my if statement");
+let age = 14;
+if (age >= 18) {
+  console.log("You can vote");
+}
+if (age < 18) {
+  console.log("You cannot vote");
+}
+console.log("after my if statement");
+*/
+
+/*
+console.log("before my if statement");
+let age = 23;
+if (age >= 18) {
+  console.log("You can vote");
+}
+console.log("after my if statement");
+if (age > 20) {
+  console.log("You are in your 20s");
+}
+*/
+
+/*
+let firstName = "Aritra";
+if (firstName == "Aritra") {
+  console.log(`Wlcome ${firstName}`);
+}
+*/
+
+/*
+let firstName = "Rahul";
+if (firstName == "Aritra") {
+  console.log(`Wlcome ${firstName}`);
+}
+*/
