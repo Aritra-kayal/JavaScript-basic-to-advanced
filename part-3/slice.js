d4e3d535b76dc7869e2a524427cadaae77a9b2c9
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-15 20:17:02 +0530

    slice method in JS

---
+++ part-3/slice.js
@@ -0,0 +1,11 @@
+let msg = "apnacollege";
+console.log(msg.slice(4, 11));
+
+let msg1 = "apnacollege";
+console.log(msg1.slice(4));
+
+let msg2 = "apnacollege";
+console.log(msg2.slice(4, msg2.length));
+
+let msg3 = "apnacollege";
+console.log(msg3.slice(-7)); //11-7=4
