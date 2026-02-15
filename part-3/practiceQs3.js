e277e71cdb1a2a91e57f92e5e88078b3f7233048
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-15 21:42:03 +0530

    practice Qs3

--- part-3/practiceQs3.js
+++ part-3/practiceQs3.js
@@ -1,3 +1,6 @@
+// let name = "ApnaCollege";
+// let newName = name.slice(4).replace("l", "t");
+// console.log(newName.replace("l", "t"));
+
 let name = "ApnaCollege";
-let newName = name.slice(4).replace("l", "t");
-console.log(newName.replace("l", "t"));
+console.log(name.slice(4).replace("l", "t").replace("l", "t"));
