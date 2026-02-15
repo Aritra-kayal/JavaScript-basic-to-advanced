9b9b636cdd3e17a1937e82ec2754e9c249bb1916
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-15 13:49:28 +0530

    to upper case and to lower case in JS

---
+++ part-3/toUpperCaseAndToLowerCase.js
@@ -0,0 +1,8 @@
+let str = "Random string";
+let newStr = str.toUpperCase();
+let newStr1 = str.toLowerCase();
+console.log(str);
+console.log(newStr);
+console.log(newStr1);
+let msg = "error";
+console.log(msg.toUpperCase());
