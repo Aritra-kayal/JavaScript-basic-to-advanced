841344dc7c56974497ec44bbb1d565d6216c90e8
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-15 18:18:46 +0530

    method chaining in JS

---
+++ part-3/methodChaining.js
@@ -0,0 +1,10 @@
+// let msg = "  hello  ";
+// console.log(msg);
+// let newMsg = msg.trim();
+// console.log(`after trime: ${newMsg}`);
+// newMsg = newMsg.toUpperCase();
+// console.log(`after upper case: ${newMsg}`);
+
+let msg = "   hello   ";
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
