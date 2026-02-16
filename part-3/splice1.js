ee2c02e7e2675654ad089e948d565087ef1a5241
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-16 20:44:22 +0530

    splice in JS

---
+++ part-3/splice1.js
@@ -0,0 +1,5 @@
+let colors = ["red", "yellow", "blue", "pink", "grey"];
+console.log(colors.splice(0, 0, "green"));
+console.log(colors);
+console.log(colors.splice(3, 1, "black"));
+console.log(colors);
