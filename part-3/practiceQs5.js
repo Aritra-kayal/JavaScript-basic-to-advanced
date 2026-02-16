c63439969190c310de48808b43f6ba94313e4f40
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-16 16:13:14 +0530

    practice Qs5

--- part-3/practiceQs5.js
+++ part-3/practiceQs5.js
@@ -1,4 +1,3 @@
 let months = ["january", "july", "march", "august"];
-let newMonths = months.splice(0, 1, "july");
-newMonths = months.splice(1, 1, "june");
+let newMonths = months.splice(0, 2, "july", "june");
 console.log(months);
