6e137e01fcc736c82c935a6ea476f41a49d86a5a
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-16 14:30:57 +0530

    slice in JS

---
+++ part-3/sliceInArray.js
@@ -0,0 +1,7 @@
+let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+console.log(colors.slice());
+console.log(colors.slice(2));
+console.log(colors.slice(2, 3));
+console.log(colors.slice(2, 4));
+console.log(colors.slice(-2));
+console.log(colors);
