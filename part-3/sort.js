b6ab56a2668620f86cc5ecd9d775ea735359b28e
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-16 15:57:28 +0530

    sort in JS

---
+++ part-3/sort.js
@@ -0,0 +1,5 @@
+let cars = ["toyota", "Gwagon", "xuv", "maruti", "bmw", "ferrari"];
+console.log(cars.sort());
+console.log(cars);
+let squares = [25, 16, 4, 49, 36, 9];
+console.log(squares.sort());
