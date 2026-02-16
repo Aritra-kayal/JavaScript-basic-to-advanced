3df72d64724ccbf67604422a326cc1bf3f0091e9
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-17 01:27:16 +0530

    Qs4

---
+++ part-3/assignment/Qs4.js
@@ -0,0 +1,15 @@
+let str = "ApNaCoLlEgE";
+let index = 3;
+if (str[index] === str[index].toLowerCase()) {
+  console.log("character is lowercase");
+} else {
+  console.log("character is not lowercase");
+}
+
+// let str = "ApNaCoLlEgE";
+// let index = 3;
+// if (str[index] < "a") {
+//   console.log("character is not lowercase");
+// } else {
+//   console.log("character is lowercase");
+// }
