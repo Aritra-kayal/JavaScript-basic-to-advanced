405b9c2c036259d95bdc3acb4654578481ac1664
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-17 01:55:33 +0530

    Qs6

---
+++ part-3/assignment/Qs6.js
@@ -0,0 +1,23 @@
+let arr = ["red", "yellow", "blue", "pink", "white"];
+let elemenName = "red";
+if (arr.includes(elemenName) === true) {
+  console.log("Exists");
+} else {
+  console.log("Not exists");
+}
+
+// let arr = ["hello", "a", 23, 64, 99, -6];
+// let item = -6;
+// if (arr.indexOf(item) == -1) {
+//   console.log("Not exists");
+// } else {
+//   console.log("Exists");
+// }
+
+// let arr = ["hello", "a", 23, 64, 99, -6];
+// let item = -6;
+// if (arr.indexOf(item) != -1) {
+//   console.log("Exists");
+// } else {
+//   console.log("Not exists");
+// }
