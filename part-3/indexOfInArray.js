a3629ce24eea410db373dcfb64728307dc5b51f9
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-16 11:53:36 +0530

    indexOf in array

---
+++ part-3/indexOfInArray.js
@@ -0,0 +1,5 @@
+let color = ["red", "yellow", "blue"];
+console.log(color.indexOf("yellow"));
+console.log(color.indexOf("red"));
+console.log(color.indexOf("green"));
+console.log(color.indexOf("Red"));
