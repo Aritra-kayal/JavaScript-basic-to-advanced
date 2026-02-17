f9c03e57502be8b10357632415d16b14d7864b31
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-18 00:57:29 +0530

    multiplication table in JS

---
+++ part-4/multiplicationTable1.js
@@ -0,0 +1,9 @@
+let n = prompt("Enter your number:");
+n = parseInt(n);
+for (let i = n; i <= n * 10; i = i + n) {
+  console.log(i);
+}
+
+for (let i = 5; i <= 50; i = i + 5) {
+  console.log(i);
+}
---
+++ part-4/multiplicationTableFor5.js
@@ -0,0 +1,9 @@
+let n = prompt("Enter your number:");
+n = parseInt(n);
+for (let i = 1; i <= 10; i++) {
+  console.log(`multiplication of ${n}x${i}=${n * i}`);
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`multiplication of ${5}x${i}=${5 * i}`);
+}
