7cdb6d43b4bfe3c29e73d5a56ed8f5bccad4b686
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-21 21:48:29 +0530

    objects literals in JS

---
+++ part-5/objectsLiterals.js
@@ -0,0 +1,20 @@
+//object literals
+let delhi = {
+  latitude: "28.7041° N",
+  longitude: "77.1025° E",
+};
+
+const student = {
+  name: "Aritra",
+  age: 20,
+  marks: 94.4,
+  city: "Delhi",
+};
+
+const item = {
+  price: 100.99,
+  discount: 50,
+  colors: ["red", "pink"],
+};
+//array
+let student2 = ["Aritra", 20, 94.4, "Delhi"];
