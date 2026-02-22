daeb6df4e46a43b9fe18f0bcc84042fb3fbfa2ff
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-22 20:23:06 +0530

    random integers

---
+++ part-5/randomIntegers.js
@@ -0,0 +1,9 @@
+//random integers 1 to 10
+let num = Math.random();
+num = num * 10;
+num = Math.floor(num);
+num = num + 1;
+console.log(num);
+//shortcut
+let ran = Math.floor(Math.random() * 10) + 1;
+console.log(ran);
