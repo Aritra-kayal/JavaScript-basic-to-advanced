6957407cbbc2928e5ed903f40d68584ebaf84f71
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-22 10:46:16 +0530

    object of object in JS

---
+++ part-5/nestedObject.js
@@ -0,0 +1,15 @@
+const classInfo = {
+  aman: {
+    grad: "A+",
+    city: "Delhi",
+  },
+  shradha: {
+    grad: "A",
+    city: "Pune",
+  },
+  karan: {
+    grad: "O",
+    city: "Mumbai",
+  },
+};
+// console.log(classInfo);
