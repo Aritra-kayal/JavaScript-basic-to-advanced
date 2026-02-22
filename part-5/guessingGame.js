5ab59e8cc4d26540b05ff95037916cd4937a434d
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-22 22:43:33 +0530

    guessing game

---
+++ part-5/guessingGame.js
@@ -0,0 +1,17 @@
+let max = prompt("enter a number for the max range:");
+const num = Math.floor(Math.random() * max) + 1;
+let guess = prompt(`enter number between 1 to ${max}`);
+while (guess != num) {
+  if (guess == "quit") {
+    console.log("user quit");
+    break;
+  }
+  if (guess == num) {
+    console.log(`you have guessed right number and that is ${guess}`);
+    break;
+  } else if (guess > num) {
+    guess = prompt("you have entered large number,please enter small number");
+  } else {
+    guess = prompt("you have entered small number,please enter large number");
+  }
+}
