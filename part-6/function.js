eef5a6a9c43bb1894881893b675f600d7bef18bd
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-23 10:28:49 +0530

    function in JS

---
+++ part-6/function.js
@@ -0,0 +1,27 @@
+//function for print hello world
+function hello() {
+  console.log("Hello World!");
+}
+hello();
+//function for print my name
+function myName() {
+  console.log("Aritra Kayal");
+}
+myName();
+//function for print 1 to 5
+function print1to5() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+print1to5();
+//funtion for adult or not
+function isAdult() {
+  let age = 18;
+  if (age >= 18) {
+    console.log("adult");
+  } else {
+    console.log("not adult");
+  }
+}
+isAdult();
