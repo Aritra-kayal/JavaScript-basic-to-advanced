200cc3068140e1db974b3d156e954b45655184cd
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-23 11:49:45 +0530

    arguments in JS

---
+++ part-6/arguments.js
@@ -0,0 +1,19 @@
+function printName(name) {
+  console.log(name);
+}
+printName("Aritra");
+
+function printInfo(name, age) {
+  console.log(`${name}'s age is ${age}`);
+}
+printInfo("Aritra", 20);
+printInfo("Raj", 19);
+printInfo("Kanak");
+printInfo(14);
+
+function sum(a, b) {
+  console.log(`sum is ${a + b}`);
+}
+sum(5, 2);
+sum(1, 2);
+sum(5, 5);
