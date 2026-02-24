dbc451661746c4188337f2ea51f95fd6983048be
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-24 11:28:12 +0530

    block scope in JS

---
+++ part-6/blockScope.js
@@ -0,0 +1,26 @@
+{
+  let a = 25;
+}
+console.log(a);
+
+{
+  const a = 25;
+}
+console.log(a);
+
+{
+  var a = 25;
+}
+console.log(a);
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log(i);
+
+let age = 25;
+if (age >= 18) {
+  let str = "adult";
+  console.log(str);
+}
+console.log(str);
