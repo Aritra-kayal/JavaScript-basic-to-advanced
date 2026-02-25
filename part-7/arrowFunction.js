e62843c7d7d0f4cf56b3d1ba8cd907c3cd9e7851
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-25 22:40:37 +0530

    arrow function in JS

---
+++ part-7/arrowFunction.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => {
+  console.log(a + b);
+};
+console.log(sum(1, 2));
+
+const cube = (n) => {
+  return n * n * n;
+};
+
+const pow = (a, b) => {
+  console.log(Math.pow(a, b));
+};
