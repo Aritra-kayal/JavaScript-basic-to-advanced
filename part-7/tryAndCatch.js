5338430007710800303109fed4ebafa1fe29d61a
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-25 21:46:27 +0530

    try and catch in JS

---
+++ part-7/tryAndCatch.js
@@ -0,0 +1,37 @@
+// console.log("hello");
+// console.log("hello");
+// console.log(a);
+// console.log("hello2");
+// console.log("hello2");
+
+// console.log("hello");
+// console.log("hello");
+// try {
+//   console.log(a);
+// } catch {
+//   console.log("caught an error.. a is not defined");
+// }
+// console.log("hello2");
+// console.log("hello2");
+
+// console.log("hello");
+// console.log("hello");
+// let a = 5;
+// try {
+//   console.log(a);
+// } catch {
+//   console.log("caught an error.. a is not defined");
+// }
+// console.log("hello2");
+// console.log("hello2");
+
+console.log("hello");
+console.log("hello");
+try {
+  console.log(a);
+} catch (err) {
+  console.log("caught an error.. a is not defined");
+  console.log(err);
+}
+console.log("hello2");
+console.log("hello2");
