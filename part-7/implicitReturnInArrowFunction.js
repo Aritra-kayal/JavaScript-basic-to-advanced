d792123f7130f10f28371f142cd2b3fa01d674cc
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-26 11:50:51 +0530

    implicit return in arrow function

---
+++ part-7/implicitReturnInArrowFunction.js
@@ -0,0 +1,11 @@
+const mul = (a, b) => a * b;
+console.log(mul(2, 3));
+
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+
+const pow = (a, b) => Math.pow(a, b);
+console.log(pow(2, 3));
+
+const cube = (n) => n * n * n;
+console.log(cube(5));
