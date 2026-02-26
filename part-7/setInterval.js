a52bd90aab10a73e8e567e148f735f37158ff113
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-26 14:27:53 +0530

    setInterval in JS

---
+++ part-7/setInterval.js
@@ -0,0 +1,8 @@
+let id = setInterval(() => {
+  console.log("Apna College");
+}, 2000);
+clearInterval(id);
+let id2 = setInterval(() => {
+  console.log("Hello World!");
+}, 3000);
+clearInterval(id2);
