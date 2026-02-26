d951c592304a913ab7a17ec86d5ac143290159ee
Aritra-kayal <aritra9434853448@gmail.com>
2026-02-26 10:47:11 +0530

    Qs1

---
+++ part-7/assignment/Qs1.js
@@ -0,0 +1,9 @@
+const arrayAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  let avg = sum / arr.length;
+  return avg;
+};
+console.log(arrayAverage([1, 2, 3, 4, 5, 6]));
