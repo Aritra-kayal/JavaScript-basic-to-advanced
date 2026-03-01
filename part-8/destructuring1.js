34a49739bbaedaf85cd1495410826c30f2dac02f
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-01 22:47:06 +0530

    destructuring in object

---
+++ part-8/destructuring1.js
@@ -0,0 +1,12 @@
+let student = {
+  name: "karan",
+  class: 9,
+  age: 14,
+  subjects: ["hindi", "english", "math", "science"],
+  username: "karan123",
+  password: 1234,
+  //   city: "pune",
+};
+const { username: user, password: pass, city: place = "kolkata" } = student;
+console.log(user, pass);
+console.log(student);
