82bf790db3be503b8c1442b0cf0afc4a79847146
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-03 19:28:13 +0530

    query selector in JS

---
+++ part-9/app1.js
@@ -0,0 +1,9 @@
+//query selector
+console.dir(document.querySelector("h1")); //select first h1 element
+console.dir(document.querySelector("#description")); //select first element with id=description
+console.dir(document.querySelector(".oldImg")); //select first element with class=oldImg
+console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("p"));
+console.dir(document.querySelectorAll("h1"));
+console.dir(document.querySelectorAll("div.box a"));
