3008ae5016c5c1279bb56e84242973a59f7662be
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-04 14:07:44 +0530

    style attribute

---
+++ part-9/app4.js
@@ -0,0 +1,9 @@
+let links = document.querySelectorAll(".box a");
+
+// for (let i = 0; i < links.length; i++) {
+//   links[i].style.color = "yellow";
+// }
+
+for (link of links) {
+  link.style.color = "red"; //inline styling
+}
