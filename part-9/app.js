7c02a2e0b2dfa4de441c1a3497a2c521c335cb81
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-05 19:24:36 +0530

    get element

--- part-9/app.js
+++ part-9/app.js
@@ -1,3 +1,4 @@
+// our first JS file for get element
 let smallImg = document.getElementsByClassName("oldImg");
 for (let i = 0; i < smallImg.length; i++) {
   smallImg[i].src = "assets/spiderman_img.png";
