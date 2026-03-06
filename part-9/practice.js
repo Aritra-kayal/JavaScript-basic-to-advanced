f2a91de27a89e436f9c0578c55fc911a2ea80113
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-06 21:20:21 +0530

    practice

---
+++ part-9/practice.js
@@ -0,0 +1,9 @@
+// let img = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < img.length; i++) {
+//   img[i].src = "assets/spiderman_img.png";
+// }
+
+// let img = document.getElementsByClassName("oldImg");
+// for (smallImg of img) {
+//   smallImg.src = "assets/creation_3.jpeg";
+// }
