7c70236014cc1b9b96dc15f367541efb8f0279ce
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-07 21:31:01 +0530

    even listener for element

---
+++ part-10/app6.js
@@ -0,0 +1,9 @@
+let para = document.querySelector("p");
+para.addEventListener("click", function () {
+  console.log("para was clicked");
+});
+
+let box = document.querySelector(".box");
+box.addEventListener("mouseenter", function () {
+  console.log("mouse inside div");
+});
