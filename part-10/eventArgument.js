e3a53ec3755f180602f396e0e7ca2bc15f39166d
Aritra-kayal <aritra9434853448@gmail.com>
2026-03-10 11:06:58 +0530

    event argument

---
+++ part-10/eventArgument.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function (event) {
+  console.log(event);
+  console.log("button clicked");
+});
+
+btn.addEventListener("dblclick", function (event) {
+  console.log(event);
+  console.log("button clicked");
+});
