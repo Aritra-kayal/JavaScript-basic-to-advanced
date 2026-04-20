b7e057c7043fdb80bfbe300d493d382ab83aedda
Aritra-kayal <aritra9434853448@gmail.com>
2026-04-20 10:53:22 +0530

    switch case

---
+++ practice/app2.js
@@ -0,0 +1,26 @@
+let day = 6;
+switch (day) {
+  case 1:
+    console.log("mon");
+    break;
+  case 2:
+    console.log("tu");
+    break;
+  case 3:
+    console.log("wen");
+    break;
+  case 4:
+    console.log("thr");
+    break;
+  case 5:
+    console.log("fri");
+    break;
+  case 6:
+    console.log("sat");
+    break;
+  case 7:
+    console.log("sun");
+    break;
+  default:
+    console.log("not a right day");
+}
