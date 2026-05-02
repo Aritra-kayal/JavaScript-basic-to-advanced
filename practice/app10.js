d34a6153f9279bc677d18d0c8b6a4a6027c4e9e9
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-02 10:43:29 +0530

    sum of two number input from user

---
+++ practice/app10.js
@@ -0,0 +1,5 @@
+let a = prompt("Enter 1st number: ");
+let b = prompt("Enter 2nd number: ");
+let sum = a + b;
+alert(`Sum of two number is ${sum}`);
+//the out put will be ab because when we use prompt in that time it consider as string not number that's why it is ab
