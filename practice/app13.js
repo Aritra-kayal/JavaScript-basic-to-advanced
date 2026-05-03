96872edf14a26923c663caf36b5e5c4e84d39b56
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-03 20:13:59 +0530

    nested for loop in nested array

---
+++ practice/app13.js
@@ -0,0 +1,11 @@
+let name = [
+  ["Aritra", "Rhaul", "Soumodeep"],
+  ["Bijay", "Amartya", "Rocky"],
+  ["Raj", "Aryan", "Dheeraj"],
+];
+for (let i = 0; i < name.length; i++) {
+  console.log(`list ${i}`);
+  for (let j = 0; j < name[i].length; j++) {
+    console.log(name[i][j]);
+  }
+}
