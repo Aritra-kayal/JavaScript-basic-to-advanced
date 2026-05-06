738f441cbf1525e8ada621a30d31d04052673cd3
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-06 11:53:46 +0530

    object of object in JavaScript

---
+++ practice/app20.js
@@ -0,0 +1,15 @@
+let studentsInfo = {
+  student1: {
+    name: "Aritra",
+    roll: 1,
+    marks: [90, 90, 90],
+    percentage: "90%",
+  },
+  student2: {
+    name: "Raj",
+    roll: 2,
+    marks: [80, 80, 80],
+    percentage: "80%",
+  },
+};
+console.log(studentsInfo);
