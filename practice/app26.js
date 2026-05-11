a832806ba0192fae621cd11819800d83eb8218f0
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-11 19:33:02 +0530

    method

---
+++ practice/app26.js
@@ -0,0 +1,18 @@
+let math = {
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+  mod: function (a, b) {
+    return a % b;
+  },
+};
+console.log(math.add(2, 3));
