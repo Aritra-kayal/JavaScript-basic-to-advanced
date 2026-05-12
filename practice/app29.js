e19fb2c3bf69d96be7bde28434ea5ce22911d62a
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-12 19:47:37 +0530

    try and catch in JavaScript

---
+++ practice/app29.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+console.log("Hello World");
+let a = 5;
+try {
+  console.log(a);
+} catch (error) {
+  console.log(error);
+  //   console.log("error");
+}
+console.log("Hello World");
+console.log("Hello World");
