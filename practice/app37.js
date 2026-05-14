060c070722517d74483d86c1fd8d8d9241568a68
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-14 20:29:27 +0530

    every method

---
+++ practice/app37.js
@@ -0,0 +1,6 @@
+let number = [1, 2, 3, 4, 5, 6, 7, 8];
+let ans = number.every((n) => n % 2 == 0);
+console.log(ans);
+let number1 = [2, 4, 6, 8];
+let ans1 = number1.every((n) => n % 2 == 0);
+console.log(ans1);
