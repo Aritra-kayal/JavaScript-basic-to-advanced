fe403c8e0cf3a06f78c4d3affc5cb777accd6680
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-14 20:33:27 +0530

    some method

---
+++ practice/app38.js
@@ -0,0 +1,6 @@
+let number = [1, 2, 3, 4, 5, 6, 7, 8];
+let ans = number.some((n) => n % 2 == 0);
+console.log(ans);
+let number1 = [1, 3, 5];
+let ans1 = number1.some((n) => n % 2 == 0);
+console.log(ans1);
