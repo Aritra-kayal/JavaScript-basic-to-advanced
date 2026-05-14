bea73844924f55d72ad0c6a8afb20365dacd62a7
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-14 20:09:58 +0530

    forEach method

---
+++ practice/app34.js
@@ -0,0 +1,30 @@
+let number = [1, 2, 3, 4, 5];
+number.forEach((el) => {
+  console.log(el);
+});
+
+let info = [
+  {
+    name: "Aritra",
+    roll: 14,
+    marks: 95,
+  },
+  {
+    name: "Rahul",
+    roll: 32,
+    marks: 90,
+  },
+  {
+    name: "Raj",
+    roll: 30,
+    marks: 85,
+  },
+  {
+    name: "Soumodeep",
+    roll: 50,
+    marks: 80,
+  },
+];
+info.forEach((student) => {
+  console.log(student.marks);
+});
