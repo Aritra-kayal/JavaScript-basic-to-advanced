31d939cec437e12f5c7612e0761c38ab62061b91
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-19 20:16:46 +0530

    switch case

---
+++ practice/app49.js
@@ -0,0 +1,14 @@
+let color = prompt("Enter color:");
+switch (color) {
+  case "red":
+    console.log("Stop");
+    break;
+  case "yellow":
+    console.log("ready");
+    break;
+  case "gree":
+    console.log("go");
+    break;
+  default:
+    console.log("wrong❌");
+}
