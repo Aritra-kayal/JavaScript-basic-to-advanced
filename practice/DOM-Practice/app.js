6362c9c4fea8b990d8f7838dce0faf4cd9ef5d5a
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-29 20:41:52 +0530

    getElementsByTagName

--- practice/DOM-Practice/app.js
+++ practice/DOM-Practice/app.js
@@ -3,10 +3,14 @@
 // console.dir(mainImgObj);
 // mainImgObj.src = "assets/creation_3.jpeg";
 
-//getElementByClassName
-let smallClass = document.getElementsByClassName("oldImg");
-for (let i = 0; i < smallClass.length; i++) {
-  //   console.dir(smallClass[i].src);
-  smallClass[i].src = "assets/creation_1.png";
-  console.log(`value of class number ${i} is changed`);
-}
+// //getElementsByClassName
+// let smallClass = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < smallClass.length; i++) {
+//   //   console.dir(smallClass[i].src);
+//   smallClass[i].src = "assets/creation_1.png";
+//   console.log(`value of class number ${i} is changed`);
+// }
+
+//getElementsByTagName
+document.getElementsByTagName("p")[1].innerText = "Aritra is a good boy";
+document.getElementsByTagName("span");
