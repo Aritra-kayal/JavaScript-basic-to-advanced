d1329af623eb7d2c25c6ab9e1567e0911c2cfc55
Aritra-kayal <aritra9434853448@gmail.com>
2026-05-29 20:24:27 +0530

    getElementByClassName

--- practice/DOM-Practice/app.js
+++ practice/DOM-Practice/app.js
@@ -1,4 +1,12 @@
-//getElementById
-let mainImgObj = document.getElementById("mainImg");
-console.dir(mainImgObj);
-mainImgObj.src = "assets/creation_3.jpeg";
+// //getElementById
+// let mainImgObj = document.getElementById("mainImg");
+// console.dir(mainImgObj);
+// mainImgObj.src = "assets/creation_3.jpeg";
+
+//getElementByClassName
+let smallClass = document.getElementsByClassName("oldImg");
+for (let i = 0; i < smallClass.length; i++) {
+  //   console.dir(smallClass[i].src);
+  smallClass[i].src = "assets/creation_1.png";
+  console.log(`value of class number ${i} is changed`);
+}
